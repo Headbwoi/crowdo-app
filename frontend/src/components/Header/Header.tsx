74e21058938c3f9913c7fba07f683cbb101169ef
headbwoi <tochukwujohn24@gmail.com>
2022-09-28 21:25:33 +0100

    added close icon

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 import Hamburger from "../../icons/Hamburger"
 import Links from "./Links"
 import MobileMenu from "./MobileMenu"
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <header className="container px-6 md:px-10 xl:px-0 relative py-7 md:py-10">
       <nav className="flex items-center justify-between">
@@ -10,7 +12,11 @@ function Header() {
           <img src={"./images/logo.svg"} alt="crappo logo" />
         </div>
         {/* hamburger */}
-        <div className="lg:hidden cursor-pointer">
+        <div
+          className="lg:hidden cursor-pointer"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+            {/* {isOpe} */}
           <Hamburger />
         </div>
         <div className="hidden lg:flex items-center space-x-10">
@@ -19,9 +25,7 @@ function Header() {
           <Links name="get started" color="white" font="normal" />
         </div>
       </nav>
-      <div>
-        <MobileMenu />
-      </div>
+      {isOpen && <MobileMenu />}
     </header>
   )
 }
