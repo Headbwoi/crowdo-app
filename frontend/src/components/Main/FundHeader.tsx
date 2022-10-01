5fbce7d9c91c0f6f24bb4964de631aa8be0dc089
headbwoi <tochukwujohn24@gmail.com>
2022-10-01 02:05:58 +0100

    Created Fund Header

---
+++ frontend/src/components/Main/FundHeader.tsx
@@ -0,0 +1,5 @@
+const FundHeader = () => {
+  return <section className="px-6 md:px-10 bg-white rounded-lg"></section>
+}
+
+export default FundHeader
--- frontend/src/components/Main/MainSection.tsx
+++ frontend/src/components/Main/MainSection.tsx
@@ -1,8 +1,10 @@
+import FundHeader from "./FundHeader"
+
 const MainSection = () => {
   return (
-    // <main className="relative w-full md:w-[45.625rem] bg-white mx-auto rounded-lg px-6"><div className="px-6 md:px-10"></div></main>
-
-    <main className="relative w-full md:w-[45.625rem] mx-auto px-6"></main>
+    <main className="relative w-full md:w-[45.625rem] mx-auto px-6">
+      <FundHeader />
+    </main>
   )
 }
 
