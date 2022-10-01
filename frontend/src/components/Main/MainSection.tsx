de32e7252043ac70a2fdcde7d1ece695afd93a58
headbwoi <tochukwujohn24@gmail.com>
2022-10-01 02:03:50 +0100

    started working on main section

---
+++ frontend/src/components/Main/MainSection.tsx
@@ -0,0 +1,9 @@
+const MainSection = () => {
+  return (
+    // <main className="relative w-full md:w-[45.625rem] bg-white mx-auto rounded-lg px-6"><div className="px-6 md:px-10"></div></main>
+
+    <main className="relative w-full md:w-[45.625rem] mx-auto px-6"></main>
+  )
+}
+
+export default MainSection
