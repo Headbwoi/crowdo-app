be5e6947378fba891ed356944606e5fe0e7b97db
headbwoi <tochukwujohn24@gmail.com>
2022-10-01 02:22:23 +0100

    added heading to fundheader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header"
+import MainSection from "./components/Main/MainSection"
 
 function App() {
   return (
     <>
       <Header />
+      <MainSection />
     </>
   )
 }
--- frontend/src/components/Main/FundHeader.tsx
+++ frontend/src/components/Main/FundHeader.tsx
@@ -1,5 +1,23 @@
 const FundHeader = () => {
-  return <section className="px-6 md:px-10 bg-white rounded-lg"></section>
+  return (
+    <section className="px-6 md:px-10 bg-white rounded-lg w-full py-9 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative">
+      <h1 className="sr-only">Name of Ptoject that Needs Funding</h1>
+      <img
+        src="/images/logo-mastercraft.svg"
+        alt="Company's Logo"
+        aria-hidden="true"
+        className="absolute top-0 left-1/2 -translate-x-1/2 h-14 w-14 -mt-7"
+      />
+      <div className="content text-center mt-5">
+        <h2 className="text-black text-lg md:text-xl lg:text-2xl font-bold mb-4 md:mb-5">
+          Mastercraft Bamboo Monitor Riser
+        </h2>
+        <p className="text-dark_Gray text-base font-normal">
+          A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default FundHeader
