f86a53ed8d0f50ebffa2feaaa7d4c39fb677f1d3
headbwoi <tochukwujohn24@gmail.com>
2022-10-02 21:37:12 +0100

    mark up styles created divider component

---
+++ frontend/src/components/Divider.tsx
@@ -0,0 +1,7 @@
+const Divider = () => {
+  return (
+    <div className="bg-dark_Gray w-20 h-[1px] md:w-[1px] md:h-20 block"></div>
+  )
+}
+
+export default Divider
--- frontend/src/components/Main/FundInfo.tsx
+++ frontend/src/components/Main/FundInfo.tsx
@@ -1,6 +1,12 @@
 const FundInfo = () => {
   return (
-    <div className="px-6 md:px-10 lg:px-12 h-56  bg-white rounded-lg w-full py-9 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative"></div>
+    <section className="px-6 md:px-10 lg:px-12 bg-white rounded-lg w-full py-9 md:py-11 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative">
+      <h2 className="sr-only">Statistics About the project</h2>
+
+      <div className="flex items-center flex-col md:flex-row md:space-x-11">
+        <div className="text-center md:text-left "></div>
+      </div>
+    </section>
   )
 }
 
