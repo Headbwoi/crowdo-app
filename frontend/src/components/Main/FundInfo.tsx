223e93fe9d35966f141f5a82cbc4c6f235449310
headbwoi <tochukwujohn24@gmail.com>
2022-10-02 22:01:55 +0100

    finished fundinfo card

--- frontend/src/components/Main/FundInfo.tsx
+++ frontend/src/components/Main/FundInfo.tsx
@@ -5,15 +5,35 @@ const FundInfo = () => {
     <section className="px-6 md:px-10 lg:px-12 bg-white rounded-lg w-full py-9 md:py-11 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative">
       <h2 className="sr-only">Statistics About the project</h2>
 
-      <div className="flex items-center flex-col md:flex-row md:space-x-11">
-        <div className="text-center md:text-left flex items-center md:items-baseline flex-col md:flex-row">
-          <p className="text-4xl text-black font-bold mb-2.5">$89,914</p>
-          <p className="text-base text-dark_Gray font-normal mb-5">
-            of $100,000 backed
-          </p>
+      <div className="flex items-center flex-col space-y-11 md:space-y-0 md:flex-row md:space-x-11  mb-6 md:mb-10">
+        <div className="text-center md:text-left flex items-center flex-col md:flex-row">
+          <div>
+            <p className="text-4xl text-black font-bold mb-2.5">$89,914</p>
+            <p className="text-base text-dark_Gray font-medium mb-5 md:mb-0 md:mr-11">
+              of $100,000 backed
+            </p>
+          </div>
           <Divider />
         </div>
+        <div className="text-center md:text-left flex items-center flex-col md:flex-row">
+          <div>
+            <p className="text-4xl text-black font-bold mb-2.5 block">5,007</p>
+            <p className="text-base text-dark_Gray font-medium mb-5 md:mb-0 md:mr-11">
+              total backers
+            </p>
+          </div>
+          <Divider />
+        </div>
+        <div className="text-center md:text-left flex items-center flex-col md:flex-row">
+          <div>
+            <p className="text-4xl text-black font-bold mb-2.5">56</p>
+            <p className="text-base text-dark_Gray font-medium mb-5 md:mb-0 md:mr-11">
+              days left
+            </p>
+          </div>
+        </div>
       </div>
+      <progress></progress>
     </section>
   )
 }
