6176f13ac7c82db057d6f91bd28f5278b88b13bd
headbwoi <tochukwujohn24@gmail.com>
2022-10-02 21:22:44 +0100

    created fundInfo Added it to MainSection component added basic styles

--- frontend/src/components/Main/MainSection.tsx
+++ frontend/src/components/Main/MainSection.tsx
@@ -1,9 +1,11 @@
 import FundHeader from "./FundHeader"
+import FundInfo from "./FundInfo"
 
 const MainSection = () => {
   return (
     <main className="relative w-full md:w-[45.625rem] mx-auto px-6 space-x-8">
       <FundHeader />
+      <FundInfo />
     </main>
   )
 }
