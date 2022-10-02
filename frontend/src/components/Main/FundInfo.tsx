de963f8b177fa55b58d3726d87e0d3625f061dc2
headbwoi <tochukwujohn24@gmail.com>
2022-10-02 21:26:39 +0100

    fixed spacing on fund sections

--- frontend/src/components/Main/FundInfo.tsx
+++ frontend/src/components/Main/FundInfo.tsx
@@ -1,6 +1,6 @@
 const FundInfo = () => {
   return (
-    <div className="px-6 md:px-10 lg:px-12 bg-white rounded-lg w-full py-9 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative"></div>
+    <div className="px-6 md:px-10 lg:px-12 h-56  bg-white rounded-lg w-full py-9 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative"></div>
   )
 }
 
--- frontend/src/components/Main/MainSection.tsx
+++ frontend/src/components/Main/MainSection.tsx
@@ -3,7 +3,7 @@ import FundInfo from "./FundInfo"
 
 const MainSection = () => {
   return (
-    <main className="relative w-full md:w-[45.625rem] mx-auto px-6 space-x-8">
+    <main className="relative w-full md:w-[45.625rem] mx-auto px-6 space-y-8">
       <FundHeader />
       <FundInfo />
     </main>
