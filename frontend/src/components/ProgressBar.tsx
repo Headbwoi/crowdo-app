5ada425de6f73b7dc7bc29b27469850e347f565f
headbwoi <tochukwujohn24@gmail.com>
2022-10-03 21:54:52 +0100

    fixed progressbar

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -25,7 +25,13 @@ const ProgressBar = ({ progress }: Props) => {
   return (
     <div className={`h-4 w-full bg-light_Gray rounded-full `}>
       <div
-        className={`h-full w-[${progress}%] bg-moderate_Cyan rounded-full`}
+        style={{
+          height: "100%",
+          width: `${progress}%`,
+          backgroundColor: "hsl(176, 50%, 47%)",
+          borderRadius: "9999px",
+          transition: "width 0.5s ease-in-out",
+        }}
       ></div>
     </div>
   )
