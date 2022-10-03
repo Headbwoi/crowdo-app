e9c12f2ef0d958546b7d629c30658b7efdd57422
headbwoi <tochukwujohn24@gmail.com>
2022-10-03 06:01:40 +0100

    added to main section

--- frontend/src/components/Main/MainSection.tsx
+++ frontend/src/components/Main/MainSection.tsx
@@ -1,3 +1,4 @@
+import AboutFund from "./AboutFund"
 import FundHeader from "./FundHeader"
 import FundInfo from "./FundInfo"
 
@@ -6,6 +7,7 @@ const MainSection = () => {
     <main className="relative w-full md:w-[45.625rem] mx-auto px-6 space-y-8">
       <FundHeader />
       <FundInfo />
+      <AboutFund />
     </main>
   )
 }
