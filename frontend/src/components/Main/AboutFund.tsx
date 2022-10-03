c71062e99fc4cc839e60feb4970b3a9375243747
headbwoi <tochukwujohn24@gmail.com>
2022-10-03 06:00:16 +0100

    created AboutFund component Edited FundInfo: removed unused css

---
+++ frontend/src/components/Main/AboutFund.tsx
@@ -0,0 +1,9 @@
+const AboutFund = () => {
+  return (
+    <section className="px-6 md:px-10 lg:px-12 bg-white rounded-lg w-full py-9  shadow-xl relative">
+      AboutFund
+    </section>
+  )
+}
+
+export default AboutFund
--- frontend/src/components/Main/FundInfo.tsx
+++ frontend/src/components/Main/FundInfo.tsx
@@ -3,7 +3,7 @@ import ProgressBar from "../ProgressBar"
 
 const FundInfo = () => {
   return (
-    <section className="px-6 md:px-10 lg:px-12 bg-white rounded-lg w-full py-9 md:py-11 mt-[10.3125rem] md:mt-[12.5rem] shadow-xl relative">
+    <section className="px-6 md:px-10 lg:px-12 bg-white rounded-lg w-full py-9 md:py-11 shadow-xl relative">
       <h2 className="sr-only">Statistics About the project</h2>
 
       <div className="flex items-center flex-col space-y-11 md:space-y-0 md:flex-row md:space-x-11  mb-6 md:mb-10">
