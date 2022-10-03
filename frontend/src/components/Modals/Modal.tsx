698ee52f25b56d2d11bc83284c07084d2f806e71
headbwoi <tochukwujohn24@gmail.com>
2022-10-03 13:10:11 +0100

    added modal heading

--- frontend/src/components/Modals/Modal.tsx
+++ frontend/src/components/Modals/Modal.tsx
@@ -2,9 +2,28 @@ type Props = {}
 
 const Modal = (props: Props) => {
   return (
-    <div className="modal bg-white w-full rounded-lg absolute -top-[8.5rem] md:-top-44 left-1/2 -translate-x-1/2 placeholder-moderate_Cyan p-5 md:p-7">
-      Modal
-    </div>
+    <>
+      <div className="modal bg-white w-full rounded-lg absolute -top-[8.5rem] md:-top-44 left-1/2 -translate-x-1/2 placeholder-moderate_Cyan p-5 md:p-7">
+        <header>
+          <div className="flex items-center justify-between">
+            <h4 className="text-lg md:text-xl text-black font-bold">
+              Back this project
+            </h4>
+            <svg
+              width="14"
+              height="15"
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-dark_Gray hover:text-black duration-200 cursor-pointer"
+            >
+              <g fill="currentColor" fillRule="evenodd">
+                <path d="M2.404.782l11.314 11.314-2.122 2.122L.282 2.904z" />
+                <path d="M.282 12.096L11.596.782l2.122 2.122L2.404 14.218z" />
+              </g>
+            </svg>
+          </div>
+        </header>
+      </div>
+    </>
   )
 }
 
