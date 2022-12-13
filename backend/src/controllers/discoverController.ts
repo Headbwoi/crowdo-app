f42bd8d3f036b0e9cb7cba83b027bb46fd2b3609
headbwoi <tochukwujohn24@gmail.com>
2022-12-13 12:01:47 +0100

    setup error handler

--- backend/src/controllers/discoverController.ts
+++ backend/src/controllers/discoverController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express"
 
-const getAllProducts = async (req: Request, res: Response) => {}
+const getAllProducts = async (req: Request, res: Response) => {
+    
+}
 
 module.exports = { getAllProducts }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import { Express } from "express"
 const dotenv = require("dotenv")
 dotenv.config()
 const express = require("express")
+const { errorHandler } = require("./middleware/errorMiddleware")
 const app: Express = express()
 const port = process.env.PORT || 5000
 
 app.use("/api/discover", require("./routes/discoverRoutes"))
 app.use("/api/getstarted", require("./routes/getStartedRoutes"))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
