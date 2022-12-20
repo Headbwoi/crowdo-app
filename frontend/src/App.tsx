1671691f8eca39cfe3a7b7f585cd71bbd891bcf4
headbwoi <tochukwujohn24@gmail.com>
2022-12-20 03:57:57 +0100

    route complete for now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,28 +7,7 @@ import {
 import { About, Discover, GetStarted, Product } from "./pages"
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/products" element={<Discover />} />
-          <Route path="/products/:id" element={<Product />} />
-          <Route path="/getstarted" element={<GetStarted />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <ScrollRestoration
-          getKey={(location, matches) => {
-            const paths = ["/products", "/about"]
-            return paths.includes(location.pathname)
-              ? // home and notifications restore by pathname
-                location.pathname
-              : // everything else by location like the browser
-                location.key
-          }}
-        />
-      </Router>
-    </>
-  )
+  return <></>
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,17 +8,34 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  ScrollRestoration,
 } from "react-router-dom"
 import { About, DashBoard, Discover, GetStarted, Product } from "./pages"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<DashBoard />}>
-      <Route path="/products" element={<Discover />} />
-      <Route path="/products/:id" element={<Product />} />
-      <Route path="/getstarted" element={<GetStarted />} />
-      <Route path="/about" element={<About />} />
-    </Route>
+    <>
+      <Route path="/" element={<DashBoard />}>
+        <Route path="/products" element={<Discover />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/getstarted" element={<GetStarted />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+      <Route
+        element={
+          <ScrollRestoration
+            getKey={(location, matches) => {
+              const paths = ["/products", "/about"]
+              return paths.includes(location.pathname)
+                ? // home and notifications restore by pathname
+                  location.pathname
+                : // everything else by location like the browser
+                  location.key
+            }}
+          />
+        }
+      />
+    </>
   )
 )
 
