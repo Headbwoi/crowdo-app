d15c2125557b79fac11fbece1938aee53735eb42
headbwoi <tochukwujohn24@gmail.com>
2022-12-20 04:02:26 +0100

    app component for base route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,3 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  ScrollRestoration,
-} from "react-router-dom"
-import { About, Discover, GetStarted, Product } from "./pages"
-
 function App() {
   return <></>
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,12 +15,13 @@ import { About, DashBoard, Discover, GetStarted, Product } from "./pages"
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<DashBoard />}>
-        <Route path="/products" element={<Discover />} />
-        <Route path="/products/:id" element={<Product />} />
-        <Route path="/getstarted" element={<GetStarted />} />
-        <Route path="/about" element={<About />} />
-      </Route>
+      <Route path="/" element={<App />} />
+      <Route path="/products" element={<Discover />} />
+      <Route path="/products/:id" element={<Product />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/getstarted" element={<GetStarted />} />
+      <Route path="/about" element={<About />} />
+
       <Route
         element={
           <ScrollRestoration
@@ -42,7 +43,6 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
       <RouterProvider router={router} />
     </AppContextProvider>
   </React.StrictMode>
