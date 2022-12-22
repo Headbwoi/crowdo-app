980080ba0f91f0af488d0671f8c9befcb72fe4b8
headbwoi <tochukwujohn24@gmail.com>
2022-12-22 02:04:53 +0100

    button link

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 type Button = {
   name: string
 }
@@ -5,7 +7,9 @@ type Button = {
 const Button = ({ name }: Button) => {
   return (
     <button className="w-36 h-12 lg:w-52 lg:h-20 rounded-full bg-darkCyan text-white font-semibold text-base md:text-2xl">
-      {name}
+      <Link to={`/${name.toString().replace(" ", "").trim().toLowerCase()}`}>
+        {name}
+      </Link>
     </button>
   )
 }
