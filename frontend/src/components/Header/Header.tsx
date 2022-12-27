afa066566321ad09572db655c2a3b161ea043974
headbwoi <tochukwujohn24@gmail.com>
2022-12-27 16:01:13 +0100

    fixed navbar to top

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -10,8 +10,8 @@ const navItems: string[] = ["home", "about", "discover", "get started"]
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <header className="container px-6 md:px-10 xl:px-0 relative py-2 md:py-3 z-50">
-      <nav className="flex items-center justify-between">
+    <header className="flex items-center justify-center  w-full h-16 md:h-20 lg:h-[5rem] bg-body/80 -mb-[1px] fixed top-0 left-0 z-50 shadow-lg px-6 md:px-10 lg:px-14 backdrop-blur-lg">
+      <nav className=" w-full flex items-center justify-between">
         <div className="logo">
           <img src="/icons/logo.svg" alt="logo" className="h-20 w-36" />
         </div>
--- frontend/src/components/HomePage/HeroSection.tsx
+++ frontend/src/components/HomePage/HeroSection.tsx
@@ -2,7 +2,7 @@ import Button from "../Button/Button"
 
 function HeroSection() {
   return (
-    <section className="flex items-center flex-col font-commissioner container  text-center gap-20 px-5 pt-14 md:pt-60 lg:w-3/4 relative z-30 overflow-hidden">
+    <section className="flex items-center flex-col font-commissioner container  text-center gap-20 px-5 pt-40 md:pt-60 lg:w-3/4 relative z-30 overflow-hidden">
       <div>
         <h1 className="font-bold text-4xl md:text-6xl lg:text-7xl text-white text-center leading-normal">
           Want To <span className="text-green_Text">Fund</span> Something But
