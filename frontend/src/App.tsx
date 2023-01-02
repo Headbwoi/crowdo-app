cb50ac8364eb0057e21329912953f7ccd729bc73
XeuxDev <tochukwujohn24@gmail.com>
2023-01-02 22:18:15 +0100

    moves routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
-import Layout from "./Layout/Layout"
-import {
-  GettingStarted,
-  HeroSection,
-  SecurityTrust,
-  StartNow,
-} from "./components/HomePage"
+import { Suspense, lazy } from "react"
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages"
+const About = lazy(() => import("./pages/About"))
+const DashBoard = lazy(() => import("./pages/DashBoard"))
+const GetStarted = lazy(() => import("./pages/GetStarted"))
+const Discover = lazy(() => import("./pages/Discover"))
+const Verify = lazy(() => import("./pages/Verify"))
+const Product = lazy(() => import("./pages/Product"))
+import { NotFound, Spinner } from "./components"
 
 function App() {
   return (
     <>
-      <Layout>
-        <div className="relative min-h-screen w-full bg-background overflow-hidden">
-          <div className="before:absolute before:w-full before:h-full before:inset-0 before:bg-[url('/images/hero-image.svg')] before:md:bg-[url('/images/hero-image-desktop.svg')] before:bg-no-repeat before:bg-cover before:bg-center before:-z-10 opacity-20 "></div>
-          <HeroSection />
-        </div>
-
-        <GettingStarted />
-        <SecurityTrust />
-        <StartNow />
-      </Layout>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} errorElement={<NotFound />} />
+          <Route path="/discover" element={<Discover />}>
+            <Route path="/discover/products/:id" element={<Product />} />
+          </Route>
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/getstarted" element={<GetStarted />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/verify" element={<Verify />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,60 +1,18 @@
-import React, { Suspense } from "react"
+import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import { AppContextProvider, UserContextProvider } from "./context"
 import "./index.css"
 
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-  ScrollRestoration,
-} from "react-router-dom"
-import { Product, Verify } from "./pages"
-import { NotFound, Spinner } from "./components"
-const About = React.lazy(() => import("./pages/About"))
-const DashBoard = React.lazy(() => import("./pages/DashBoard"))
-const GetStarted = React.lazy(() => import("./pages/GetStarted"))
-const Discover = React.lazy(() => import("./pages/Discover"))
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<App />} errorElement={<NotFound />} />
-      <Route path="/discover" element={<Discover />}>
-        <Route path="/discover/products/:id" element={<Product />} />
-      </Route>
-      <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/getstarted" element={<GetStarted />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/verify" element={<Verify />} />
-
-      <Route
-        element={
-          <ScrollRestoration
-            getKey={(location, matches) => {
-              const paths = ["/products", "/about"]
-              return paths.includes(location.pathname)
-                ? // home and notifications restore by pathname
-                  location.pathname
-                : // everything else by location like the browser
-                  location.key
-            }}
-          />
-        }
-      />
-    </>
-  )
-)
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppContextProvider>
       <UserContextProvider>
-        <Suspense fallback={<Spinner />}>
-          <RouterProvider router={router} />
-        </Suspense>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </UserContextProvider>
     </AppContextProvider>
   </React.StrictMode>
