571e5b748514d1db7e19f53084a49768664f47b0
XeuxDev <tochukwujohn24@gmail.com>
2023-01-02 22:19:42 +0100

    moves routes to App.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import Layout from "../Layout/Layout"
+import {
+  GettingStarted,
+  HeroSection,
+  SecurityTrust,
+  StartNow,
+} from "../components/HomePage"
+
+function Home() {
+  return (
+    <>
+      <Layout>
+        <div className="relative min-h-screen w-full bg-background overflow-hidden">
+          <div className="before:absolute before:w-full before:h-full before:inset-0 before:bg-[url('/images/hero-image.svg')] before:md:bg-[url('/images/hero-image-desktop.svg')] before:bg-no-repeat before:bg-cover before:bg-center before:-z-10 opacity-20 "></div>
+          <HeroSection />
+        </div>
+
+        <GettingStarted />
+        <SecurityTrust />
+        <StartNow />
+      </Layout>
+    </>
+  )
+}
+
+export default Home
