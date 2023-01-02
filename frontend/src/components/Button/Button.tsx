41b4673d25c463619b5a37bbada446fa61ef435a
XeuxDev <tochukwujohn24@gmail.com>
2023-01-02 04:58:39 +0100

    adds verify page base content reduces button text on mobile

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ type Button = {
 
 const Button = ({ name, link }: Button) => {
   return (
-    <button className="w-36 h-12 lg:w-40 lg:h-14 rounded-full bg-darkCyan text-white font-semibold text-base md:text-lg hover:bg-moderate_Cyan duration-300">
+    <button className="w-32 h-12 lg:w-40 lg:h-14 rounded-full bg-darkCyan text-white font-semibold text-sm md:text-lg hover:bg-moderate_Cyan duration-300">
       <Link
         to={`/${link ?? name.toString().replace(" ", "").trim().toLowerCase()}`}
       >
--- frontend/src/pages/Verify.tsx
+++ frontend/src/pages/Verify.tsx
@@ -1,5 +1,22 @@
+import Layout from "../Layout/Layout"
+import { Button } from "../components"
+
 function Verify() {
-  return <div>Verify</div>
+  return (
+    <>
+      <Layout>
+        <section className="flex justify-center items-center min-h-screen ">
+          <>
+            <h1>Verify your email</h1>
+          </>
+          <div>
+            <p>verification link has been sent to your email 👍👍. </p>
+            <Button name="verify your email" />
+          </div>
+        </section>
+      </Layout>
+    </>
+  )
 }
 
 export default Verify
