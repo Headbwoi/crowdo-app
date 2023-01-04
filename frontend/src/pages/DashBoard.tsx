7aa59a3e7c0b5713d521fb5bde146631b032fcb1
XeuxDev <tochukwujohn24@gmail.com>
2023-01-04 22:15:08 +0100

    Dashboard : if there's no user then go to login else remain at dashboard

--- frontend/src/pages/DashBoard.tsx
+++ frontend/src/pages/DashBoard.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
 import Layout from "../Layout/Layout"
 import { MainSection } from "../components"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useNavigate } from "react-router-dom"
 
 function DashBoard() {
+  const { state } = useAuthContext()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    state.user?.token == "" ? navigate("/getstarted") : navigate("/dashboard")
+  }, [state.user])
+
   return (
     <Layout>
       <div className="bg-light_Gray">
