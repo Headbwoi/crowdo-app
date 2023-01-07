a2d9829b377e274864607ed73dcccbd056363d9e
headbwoi <tochukwujohn24@gmail.com>
2023-01-07 05:02:23 +0100

    add a profile component
    with information about the user fetched from state with logout functionality when sign out is clicked

---
+++ frontend/src/components/Header/Profile.tsx
@@ -0,0 +1,101 @@
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useLogout } from "../../hooks/useLogout"
+
+const Profile = () => {
+  const { logout } = useLogout()
+  const { state } = useAuthContext()
+  return (
+    <>
+      <div className="flex items-center md:order-2">
+        <button
+          type="button"
+          className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+          id="user-menu-button"
+          aria-expanded="false"
+          data-dropdown-toggle="user-dropdown"
+          data-dropdown-placement="bottom"
+        >
+          <span className="sr-only">Open user menu</span>
+          <img
+            className="w-8 h-8 rounded-full"
+            src={`https://api.multiavatar.com/${state.user?.firstName}%20${state.user?.lastName}.svg`}
+            alt={`${state.user?.firstName} avatar`}
+          />
+        </button>
+        {/* <!-- Dropdown menu --> */}
+        <div
+          className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+          id="user-dropdown"
+        >
+          <div className="px-4 py-3">
+            <span className="block text-sm text-gray-900 dark:text-white">
+              {state.user?.firstName}
+              {state.user?.lastName}
+            </span>
+            <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+              {state.user?.email}
+            </span>
+          </div>
+          <ul className="py-1" aria-labelledby="user-menu-button">
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Settings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Earnings
+              </a>
+            </li>
+            <li onClick={() => logout()}>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Sign out
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button
+          data-collapse-toggle="mobile-menu-2"
+          type="button"
+          className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mobile-menu-2"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Profile
